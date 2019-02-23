1f2b6a4f24883ea9e07b995e4b273cdd89742893
Norbert Koszo <k.norbi@gmail.com>
2019-02-23 18:30:24 +0100

    remove debug prints

--- app/pwmwebserver.cpp
+++ app/pwmwebserver.cpp
@@ -10,22 +10,12 @@ GpioPWM gpiopwm;
 
 void onAjaxLight(HttpRequest& request, HttpResponse& response)
 {
-	debugf("onAjaxLight");
-    debugf("path: %s", request.uri.Path.c_str());
 	if (request.method == HTTP_POST) {
-		debugf("POST");
-		debugf("%s", request.getHeader("content-type").c_str());
-        //debugf("HEADER: \"%s\"\n", request.getHeader());
 		StaticJsonBuffer<200> jsonBuffer;
 		JsonObject& root = jsonBuffer.parseObject(request.getBody());
-		root.prettyPrintTo(Serial);
         for (size_t i = 0; i < root["pwm"].asArray().size(); ++i) {
-			debugf("%d, %d", i, (uint32)root["pwm"][i]);
             gpiopwm.setDuty(i, (uint32)root["pwm"][i]);
         }
-		//int pwm = ((uint32)root["pwm"])*10;
-		//debugf("pwm = %d", pwm);
-        //HW_pwm.analogWrite(2, pwm);
 	}
     response.sendString("");
 }
